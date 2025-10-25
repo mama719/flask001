74c33be44c19c9222bbd700cb4cb990e4c377a9f
mama719 <ciegalaxy2@gmail.com>
2025-10-25 09:04:36 +0000

    Update route.py

--- route.py
+++ route.py
@@ -44,8 +44,9 @@ def token_required(f) :
 @token_required 
 def profil(cu) :
     return profile(cu)
+    
+with app.app_context() :
+    db.create_all() 
 
 if __name__ == "__main__" :
-    with app.app_context() :
-        db.create_all() 
     app.run(debug=True , port=5000)
